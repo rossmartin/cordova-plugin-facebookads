0fb29cc073d402c50f73895d70122ad771eb8270
Ross Martin <rmartin311@gmail.com>
2020-12-01 00:23:56 -0600

    Implement removeBanner

--- src/ios/FacebookAdPlugin.m
+++ src/ios/FacebookAdPlugin.m
@@ -7,27 +7,7 @@
 @end
 
 
-// ------------------------------------------------------------------
-
 @implementation FacebookAdPlugin
-
-//- (void) setOptions:(CDVInvokedUrlCommand *)command
-//{
-//    NSDictionary* options = [command.arguments objectAtIndex:0];
-//
-//    bool isTesting = [options objectForKey:@"isTesting"];
-//
-//    if (isTesting) {
-//        NSString* str = [options objectForKey:OPT_DEVICE_HASH];
-//        if(str && [str length] > 0) {
-//            NSLog(@"set device hash: %@", str);
-//            [FBAdSettings addTestDevice:str];
-//        }
-//    }
-//
-//
-//    [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"success"] callbackId:command.callbackId];
-//}
 
 - (void) createBanner:(CDVInvokedUrlCommand *)command
 {
@@ -52,28 +32,21 @@
   [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"success"] callbackId:command.callbackId];
 }
 
-- (void) __pauseAdView:(UIView*)view
+- (void)removeBanner:(CDVInvokedUrlCommand *)command
 {
-    if([view isKindOfClass:[FBAdView class]]) {
-        //FBAdView* ad = (FBAdView*) view;
-        // [ad pause];
-    }
-}
-
-- (void) __resumeAdView:(UIView*)view
-{
-    if([view isKindOfClass:[FBAdView class]]) {
-        //FBAdView* ad = (FBAdView*) view;
-        // [ad resume];
-    }
-}
-
-- (void) __destroyAdView:(UIView*)view
-{
-    if([view isKindOfClass:[FBAdView class]]) {
-        FBAdView* ad = (FBAdView*) view;
-        ad.delegate = nil;
-    }
+  if (self.adView) {
+    [self.adView removeFromSuperview];
+    self.adView.delegate = nil;
+    self.adView = nil;
+    
+    CGRect superViewBounds = self.webView.superview.bounds;
+    CGRect window = CGRectMake(superViewBounds.origin.x, superViewBounds.origin.y, superViewBounds.size.width, superViewBounds.size.height);
+    
+    self.webView.bounds = window;
+    self.webView.frame = window;
+  }
+  
+  [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"success"] callbackId:command.callbackId];
 }
 
 #pragma mark FBAdViewDelegate implementation
@@ -89,6 +62,7 @@
 - (void)adViewDidLoad:(FBAdView *)adView
 {
     [self fireEvent:NULL event:@"adViewDidLoad" withData:NULL];
+  
     if (self.adView && self.adView.isAdValid) {
       CGRect webViewBounds = self.webView.bounds;
       NSLog(@"webViewBounds, %f by %f", webViewBounds.size.width, webViewBounds.size.height);
